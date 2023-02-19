3924a9e5194fcd002c38c729300d93324dcc9214
Jasneet Dua <jasneetdua96@gmail.com>
2023-02-19 19:59:00 +0530

    added profile pic component

---
+++ force-app/main/default/lwc/portfolioPicture/portfolioPicture.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import Profile_Image_Url from '@salesforce/label/c.Profile_Image_Url';
+
+export default class PortfolioPicture extends LightningElement {
+
+    label = {
+        Profile_Image_Url,
+    }
+}
